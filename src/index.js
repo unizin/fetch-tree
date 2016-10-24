0c1c674ff41b01c2b4f0f5349629276e6feb225f
Asa Ayers <Asa@AsaAyers.com>
2016-10-24 13:30:22 -0700

    Remove deprecated lazy and preload

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import group from './node_types/group'
 import loader from './node_types/loader'
 import selector from './node_types/selector'
 import depends from './node_types/depends'
-import preload from './node_types/preload'
 import debug from './node_types/debug'
 import virtual from './node_types/virtual'
-import lazy from './node_types/lazy'
 import entity from './node_types/entity'
 import withProps from './node_types/with-props'
 import fromProps from './node_types/from-props'
@@ -15,7 +13,7 @@ import reducer from './actions-reducer'
 
 import { register as registerNodeType } from './processor.js'
 
-export { group, loader, selector, depends, preload, reducer, debug, virtual, lazy, entity, withProps, fromProps }
+export { group, loader, selector, depends, reducer, debug, virtual, entity, withProps, fromProps }
 
 export { registerNodeType }
 export default component
