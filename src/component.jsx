d4bd65a125f5d49a01ab5793ade45c8cc9b039e2
Daniel Sato <daniel.sato@unizin.org>
2018-05-04 09:36:24 -0500

    Apply proptypes codemod

--- src/component.jsx
+++ src/component.jsx
@@ -1,111 +1,107 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import loaderContext from './loader_context'
-import processor from './processor'
+import PropTypes from 'prop-types';
+import React from "react";
+import { connect } from "react-redux";
+import loaderContext from "./loader_context";
+import processor from "./processor";
 
-const getDisplayName = Component => (
+const getDisplayName = Component =>
   Component.displayName ||
   Component.name ||
-  (typeof Component === 'string' ? Component : 'Component')
-)
+  (typeof Component === "string" ? Component : "Component");
 
-
-const contextShape = React.PropTypes.shape({
-    execute: React.PropTypes.func.isRequired,
-    hasExecuted: React.PropTypes.func.isRequired,
-})
+const contextShape = PropTypes.shape({
+  execute: PropTypes.func.isRequired,
+  hasExecuted: PropTypes.func.isRequired
+});
 
 // These need to get passed as props and we need to avoid name conflicts with
 // any props the user is passing through to Component. Using the dashes and
 // colons ensures that we can't accidentally conflict with JSX props.
-const IS_READY = '--loader:is-ready'
-const ACTION_QUEUE = '--loader:action-queue'
-const DISPATCH = '--loader:dispatch'
+const IS_READY = "--loader:is-ready";
+const ACTION_QUEUE = "--loader:action-queue";
+const DISPATCH = "--loader:dispatch";
 
-export default function ({ component: Component, busy: Busy, resources, mapDispatchToProps = {} }) {
-    const displayName = `Loader(${getDisplayName(Component)})`
+export default function({
+  component: Component,
+  busy: Busy,
+  resources,
+  mapDispatchToProps = {}
+}) {
+  const displayName = `Loader(${getDisplayName(Component)})`;
 
-    if (resources.TYPE !== 'group') {
-        if (resources.TYPE === 'debug' && resources.child.TYPE === 'group') {
-            // It's ok to wrap the group in a debug node
-        } else {
-            throw new Error('resources must be a `group()`')
-        }
+  if (resources.TYPE !== "group") {
+    if (resources.TYPE === "debug" && resources.child.TYPE === "group") {
+      // It's ok to wrap the group in a debug node
+    } else {
+      throw new Error("resources must be a `group()`");
+    }
+  }
+
+  class LoaderComponent extends React.Component {
+    static displayName = displayName;
+
+    static childContextTypes = {
+      loaderContext: contextShape
+    };
+
+    static contextTypes = {
+      loaderContext: contextShape
+    };
+
+    constructor(props, context) {
+      super(props, context);
+      this.loaderContext = loaderContext(context.loaderContext);
     }
 
-    class LoaderComponent extends React.Component {
-        static displayName = displayName
-
-        static childContextTypes = {
-            loaderContext: contextShape,
-        }
-
-        static contextTypes = {
-            loaderContext: contextShape,
-        }
-
-        constructor(props, context) {
-            super(props, context)
-            this.loaderContext = loaderContext(context.loaderContext)
-        }
-
-        getChildContext() {
-            return {
-                loaderContext: this.loaderContext,
-            }
-        }
-
-        componentDidMount() {
-            const {
-                [ACTION_QUEUE]: actionQueue,
-                [DISPATCH]: dispatch,
-            } = this.props
-            this.loaderContext.execute(dispatch, actionQueue)
-        }
-
-        componentDidUpdate() {
-            const {
-                [ACTION_QUEUE]: actionQueue,
-                [DISPATCH]: dispatch,
-            } = this.props
-            this.loaderContext.execute(dispatch, actionQueue)
-        }
-
-        render() {
-            const {
-                [IS_READY]: isReady,
-                [ACTION_QUEUE]: actionQueue, // eslint-disable-line no-unused-vars
-                ...props,
-            } = this.props
-
-            if (!isReady) {
-                if (Busy) {
-                    return (<Busy />)
-                }
-
-                return (<noscript></noscript>)
-            }
-
-            return (
-                <Component {...props} />
-            )
-        }
+    getChildContext() {
+      return {
+        loaderContext: this.loaderContext
+      };
     }
 
-    function mapStateToProps(state, props) {
-        const { isReady, actionQueue, value } = processor(resources, state, props)
+    componentDidMount() {
+      const { [ACTION_QUEUE]: actionQueue, [DISPATCH]: dispatch } = this.props;
+      this.loaderContext.execute(dispatch, actionQueue);
+    }
 
-        return {
-            ...value,
-            [IS_READY]: isReady,
-            [ACTION_QUEUE]: actionQueue,
+    componentDidUpdate() {
+      const { [ACTION_QUEUE]: actionQueue, [DISPATCH]: dispatch } = this.props;
+      this.loaderContext.execute(dispatch, actionQueue);
+    }
+
+    render() {
+      const {
+        [IS_READY]: isReady,
+        [ACTION_QUEUE]: actionQueue, // eslint-disable-line no-unused-vars
+        ...props
+      } = this.props;
+
+      if (!isReady) {
+        if (Busy) {
+          return <Busy />;
         }
-    }
 
-    if (typeof mapDispatchToProps === 'function') {
-        throw new Error('not implemented')
-    }
-    mapDispatchToProps[DISPATCH] = (action) => action
+        return <noscript />;
+      }
 
-    return connect(mapStateToProps, mapDispatchToProps)(LoaderComponent)
+      return <Component {...props} />;
+    }
+  }
+
+  function mapStateToProps(state, props) {
+    const { isReady, actionQueue, value } = processor(resources, state, props);
+
+    return {
+      ...value,
+      [IS_READY]: isReady,
+      [ACTION_QUEUE]: actionQueue
+    };
+  }
+
+  if (typeof mapDispatchToProps === "function") {
+    throw new Error("not implemented");
+  }
+  mapDispatchToProps[DISPATCH] = action => action;
+
+  return connect(mapStateToProps, mapDispatchToProps)(LoaderComponent);
 }
